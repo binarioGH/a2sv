555c999881796c6c8ebc36916e3b2af325e9eb7d
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2018-05-07 22:52:57 -0500

    Add files via upload

---
+++ a2sv-py3/module/C_display.py
@@ -0,0 +1,14 @@
+#===========================
+#              Display option                  #
+def showDisplay(mode,string):
+	if(mode == 0):
+		print(string)
+#===========================
+def setDisplay(mode):
+	displayMode = mode
+
+#global displayMode
+#displayMode = 0
+#global displayMode
+#global displayMode
+#===========================
